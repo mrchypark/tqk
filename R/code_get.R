32174c2f20a7a972023a3803c7440426ebf2fd33
chanyub Park <mrchypark@gmail.com>
2020-11-10 20:32:29 +0900

    done

--- R/code_get.R
+++ R/code_get.R
@@ -11,33 +11,33 @@
 #' @importFrom dplyr bind_rows as_tibble select
 #' @importFrom purrr map_chr
 
-code_get <- function(){
+code_get <- function() {
 
-  httr::POST("https://kind.krx.co.kr/corpgeneral/corpList.do?method=download&pageIndex=1&currentPageSize=5000&comAbbrv=&beginIndex=&orderMode=3&orderStat=D&isurCd=&repIsuSrtCd=&searchCodeType=&marketType=&searchType=13&industry=&fiscalYearEnd=all&comAbbrvTmp=&location=all") %>%
+  httr::POST(
+    "https://kind.krx.co.kr/corpgeneral/corpList.do?method=download&pageIndex=1&currentPageSize=5000&comAbbrv=&beginIndex=&orderMode=3&orderStat=D&isurCd=&repIsuSrtCd=&searchCodeType=&marketType=&searchType=13&industry=&fiscalYearEnd=all&comAbbrvTmp=&location=all"
+  ) %>%
     httr::content() %>%
     rawToChar() %>%
     xml2::read_html() %>%
     rvest::html_nodes("td") %>%
     rvest::html_text() %>%
-    stringr::str_squish() ->
-    cont
-
-  cont %>%
-    matrix(ncol=9, byrow = T) %>%
+    stringr::str_squish() %>%
+    matrix(ncol = 9, byrow = T) %>%
     dplyr::as_tibble() %>%
-    dplyr::select(1,2) ->
+    dplyr::select(1, 2) ->
     dat
 
-  httr::POST("https://kind.krx.co.kr/corpgeneral/corpList.do?method=searchCorpList&pageIndex=1&currentPageSize=5000&comAbbrv=&beginIndex=&orderMode=3&orderStat=D&isurCd=&repIsuSrtCd=&searchCodeType=&marketType=&searchType=13&industry=&fiscalYearEnd=all&comAbbrvTmp=&location=all") %>%
+  httr::POST(
+    "https://kind.krx.co.kr/corpgeneral/corpList.do?method=searchCorpList&pageIndex=1&currentPageSize=5000&comAbbrv=&beginIndex=&orderMode=3&orderStat=D&isurCd=&repIsuSrtCd=&searchCodeType=&marketType=&searchType=13&industry=&fiscalYearEnd=all&comAbbrvTmp=&location=all"
+  ) %>%
     httr::content() %>%
     rvest::html_nodes("td.first") %>%
-    purrr::map_chr(
-      ~ .x %>% rvest::html_nodes("img") %>%
-        .[1] %>%
-        rvest::html_attr("alt")
-    ) -> market
+    xml2::xml_find_first("img") %>%
+    rvest::html_attr("alt") ->
+      market
 
-  dat <- dplyr::bind_cols(dat, market)
-  names(dat) <- c("name","code","market")
-  return(dat)
+    dplyr::tibble(market = market,
+                  name = dat$V1,
+                  code = dat$V2) %>%
+    return()
 }
