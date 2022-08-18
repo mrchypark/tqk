6eea3120dfae475e23eff8e8361e3b5e272a80a2
chanyub Park <mrchypark@gmail.com>
2022-08-18 13:44:06 +0900

    add code get, tqk get, SHANK explain

--- data-raw/SHANK.R
+++ data-raw/SHANK.R
@@ -6,16 +6,16 @@ library(tidyr)
 sam <- tqk_get("005930", to = "2017-09-07")
 hyun <- tqk_get("005380", to = "2017-09-07")
 amore <- tqk_get("090430", to = "2017-09-07")
-nhn <- tqk_get("035420", to = "2017-09-07")
+naver <- tqk_get("035420", to = "2017-09-07")
 kakao <- tqk_get("035720", to = "2017-09-07")
 
 sam$symbol <- "SS"
 hyun$symbol <- "HYD"
 amore$symbol <- "AMP"
-nhn$symbol <- "NVR"
+naver$symbol <- "NVR"
 kakao$symbol <- "KKO"
 
-SHANK <- rbind(sam, hyun, amore, nhn, kakao)
+SHANK <- rbind(sam, hyun, amore, naver, kakao)
 SHANK <- SHANK %>% select(symbol, everything())
 
 usethis::use_data(SHANK, overwrite = TRUE)
